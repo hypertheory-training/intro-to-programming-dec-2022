e59992e947a239df48b1f374e3ec76c1647ce194
Jeffry Gonzalez <jeff@hypertheory.com>
2022-12-07 10:35:22 -0500

    done with flags example

--- BankingSolution/Banking.UnitTests/GoldAccountBenefits.cs
+++ BankingSolution/Banking.UnitTests/GoldAccountBenefits.cs
@@ -7,13 +7,17 @@ public class GoldAccountBenefits
     [Fact]
     public void GetBonusOnDeposit()
     {
+        // Give (Context)
         var account = new BankAccount();
+        account.AccountType = BankAccountType.Gold;
         var openingBalance = account.GetBalance();
         var amountToDeposit = 100M;
         var expectedBonus = 10M;
 
+        // When
         account.Deposit(amountToDeposit);
-
+        
+        // Then
         Assert.Equal(openingBalance + amountToDeposit + expectedBonus, 
             account.GetBalance());
 
