44a684f4c93203b5584c6d939850c032aec080b9
Jeffry Gonzalez <jeff@hypertheory.com>
2022-12-09 14:58:48 -0500

    template method

--- CSharpSyntaxSolution/CSharpSyntax/NumberFilter.cs
+++ CSharpSyntaxSolution/CSharpSyntax/NumberFilter.cs
@@ -1,24 +1,26 @@
-﻿namespace CSharpSyntax;
+﻿using System.Reflection.Metadata;
+
+namespace CSharpSyntax;
 
 
 public class NumberFilter
 {
     public List<int> FilterOutEvens(List<int> numbers)
     {
-        return Filter(numbers);
+        return Filter(numbers, new IsOddFilterPredicate());
     }
     public List<int> FilterOutOdds(List<int> numbers)
     {
-        return Filter(numbers);
+        return Filter(numbers, new IsEvenPredicate());
     }
 
-    private List<int> Filter(List<int> numbers)
+    private List<int> Filter(List<int> numbers, FilterPredicate pred)
     {
         var results = new List<int>();
         foreach (var number in numbers)
         {
           
-            if (IsOdd(number))
+            if (pred.IsMatch(number))
             {
                 results.Add(number);
             }
@@ -33,3 +35,23 @@ public class NumberFilter
     }
 }
 
+public interface FilterPredicate
+{
+    bool IsMatch(int x);
+}
+
+public class IsOddFilterPredicate : FilterPredicate
+{
+    public bool IsMatch(int x)
+    {
+        return x %2 != 0;
+    }
+}
+
+public class IsEvenPredicate : FilterPredicate
+{
+    public bool IsMatch(int x)
+    {
+        return x % 2 == 0;
+    }
+}
